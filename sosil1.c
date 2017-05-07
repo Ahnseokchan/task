035f1f9bbcdde7ade5920005b62950454bd7cf95
ahn6219 <kyudsa@naver.com>
2017-05-07 12:38:28 +0900

    task1

---
+++ sosil1.c
@@ -0,0 +1,6 @@
+#include "sosil1.h"
+
+void func1()
+{
+	printf("If egg is broken by outside force,\n");
+}
---
+++ sosil5.c
@@ -0,0 +1,6 @@
+#include "sosil5.h"
+
+void func5()
+{
+	printf("Great things always begin from inside.\n");
+}
